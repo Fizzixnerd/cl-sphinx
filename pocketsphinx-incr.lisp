f0df13eb55ae1a1a38ac234286ac4db224a1785d
Matt Walker <matt.g.d.walker@gmail.com>
2013-10-27 13:58:05 -0400

    Commit just before moving to an ASDF-based package format.
     On branch master
    
    	new file:   config.txt
    	*Changed the name of options.txt and modified it to make it
    	easier to parse.
    
    	new file:   parse-options-file.lisp
    	*Finished work here, will have to migrate to a more complete
    	solution later, but this works for now.
    	*Will likely be renamed in the future.
    
    	modified:   pocketsphinx-incr.lisp
    	*Added CLOS wrappers for `HYPOTHESIS', `DECODER', and `CONFIG'.
    	*Added generic functions with methods specialized on these
    	types.
    	*Completed a working example using my own speech-input file.
    	*Wrote macro wrappers over `CFFI:DEFCFUN' to disable
    	floating-point traps in SBCL.
    	*Added finalizers from TRIVIAL-GARBAGE to remove need for
    	manual memory management of `DECODERS'.

--- pocketsphinx-incr.lisp
+++ pocketsphinx-incr.lisp
@@ -2,17 +2,21 @@
 
 (require 'asdf)
 
+(asdf:load-system :cffi)
+(asdf:load-system :alexandria)
+(asdf:load-system :trivial-garbage)
+
 (defpackage :pocketsphinx
   (:use :common-lisp
 	:cffi
-	:alexandria)
+	:alexandria
+	:trivial-garbage)
   (:nicknames :ps)
   (:export libpocketsphinx
 	   libsphinxbase
 	   ps-decoder-t
 	   cmd-ln-t
 	   arg-t
-	   cmd-ln-init-m
 	   cmd-ln-str-r
 	   ps-args
 	   ps-init
@@ -23,9 +27,6 @@
 	   defcfun))
 
 (in-package :pocketsphinx)
-
-(asdf:load-system :cffi)
-(asdf:load-system :alexandria)
 
 (define-foreign-library libpocketsphinx
   (:unix (:or "libpocketsphinx.so.1" "libpocketsphinx.so"))
@@ -42,6 +43,7 @@
 (defctype cmd-ln-t :pointer)
 (defctype arg-t :pointer)
 (defctype file :pointer)
+(defctype str-array :pointer)
 
 ;; helpers
 (defmacro without-float-traps (&body body)
@@ -61,8 +63,14 @@ The DEFUN or DEFMACRO form within the form returned by a call
 to (MACROEXPAND-1 '(CFFI:DEFCFUN ...)).
 
 The form(s) before the located form."
-  (values (lastcar macroexpanded-defcfun-form)
-	  (butlast macroexpanded-defcfun-form)))
+  ;; Make this throw an error actually, and check instead for
+  ;; `DEFMACRO' or `PROGN'
+  (cond ((string= (car macroexpanded-defcfun-form) 'progn)
+	 (values (lastcar macroexpanded-defcfun-form)
+		 (butlast macroexpanded-defcfun-form)))
+	((string= (car macroexpanded-defcfun-form) 'defmacro)
+	 (values macroexpanded-defcfun-form
+		 '()))))
 
 ;; TESTED
 (defun locate-defun/defmacro-body (macroexpanded-defcfun-form)
@@ -73,10 +81,15 @@ The form(s) before the located form."
 	    (subseq macroexpanded-defcfun-form 0 forms-before-body))))
 
 ;; TESTED
-(defun wrap-form (wrapper-form &rest forms)
-  (append wrapper-form forms))
+(defun wrap-form (wrapper-form form)
+  (if wrapper-form
+      (append wrapper-form `(,form))
+      form))
 
 ;; TESTED
+;; FIXME: NEED TO FIX THIS FOR MACROS, IT CURRENTLY DOESN'T WORK AS
+;;        INTENDED (just wraps the macro in the _function call_,
+;;        doesn't make a macro.
 (defmacro defcfun (name-and-options return-type &rest args)
   (multiple-value-bind (defun/defmacro-form prior-forms)
       (locate-defun/defmacro-form (macroexpand-1
@@ -92,17 +105,10 @@ The form(s) before the located form."
   (pathname :string)
   (mode :string))
 
-;; (defmacro defcfun (name-and-options return-type &rest args)
-;;   ;; TODO: Maybe add an only-once form here?
-;;   (let ((defcfun-macro-expansion
-;; 	 (macroexpand-1 `(cffi:defcfun ,name-and-options ,return-type ,@args))))
-;;     (append (butlast defcfun-macro-expansion)
-;; 	    (append (subseq (car (last defcfun-macro-expansion)) 0 3)
-;; 		    (list (list 'without-float-traps
-;; 				(subseq (car (last defcfun-macro-expansion)) 3)))))))
-		       
-;; sphinxbase functions
+(defcfun "fclose" :void
+  (filehandle file))
 
+;; sphinxbase functions
 (defcfun "cmd_ln_init" cmd-ln-t
   (inout-cmdln cmd-ln-t)
   (defn arg-t)
@@ -113,11 +119,57 @@ The form(s) before the located form."
   (cmdln cmd-ln-t)
   (name :string))
 
+(defcfun "cmd_ln_str_list_r" str-array
+  (cmdln cmd-ln-t)
+  (name :string))
+
+(defcfun "cmd_ln_int_r" :long
+  (cmdln cmd-ln-t)
+  (name :string))
+
+(defcfun "cmd_ln_float_r" :double
+  (cmdln cmd-ln-t)
+  (name :string))
+
+;; Is set as a macro in cmd_ln.h
+(defun cmd-ln-boolean-r (cmdln name)
+  (not (zerop (cmd-ln-int-r cmdln name))))
+
+(defcfun "cmd_ln_set_str_r" :void
+  (cmdln cmd-ln-t)
+  (name :string)
+  (str :string))
+
+(defcfun "cmd_ln_set_int_r" :void
+  (cmdln cmd-ln-t)
+  (name :string)
+  (int :long))
+
+(defcfun "cmd_ln_set_float_r" :void
+  (cmdln cmd-ln-t)
+  (name :string)
+  (int :long))
+
+(defun cmd-ln-set-boolean-r (cmdln name bool)
+  (cmd-ln-set-int-r cmdln name (not (zerop bool))))
+
+(defcfun "cmd_ln_exists_r" :int
+  (cmdln cmd-ln-t)
+  (name :string))
+
+(defcfun "cmd_ln_print_help_r" :void
+  (cmdln cmd-ln-t)
+  (fp file)
+  (defn arg-t))
+
 ;; pocketsphinx functions
 (defcfun "ps_args" arg-t)
 
 (defcfun "ps_init" ps-decoder-t
   (config cmd-ln-t))
+
+(defcfun "ps_free" :int
+  (ps ps-decoder-t))
 
 (defcfun "ps_decode_raw" :int
   (ps ps-decoder-t)
@@ -130,6 +182,114 @@ The form(s) before the located form."
   (out-best-score :pointer)
   (out-uttid :pointer))
 
+(defcfun "ps_get_config" cmd-ln-t
+  (ps ps-decoder-t))
+
+;; CLOS API
+(defclass config ()
+  ;; TODO: Document
+  ;; TODO: In the initialize-instance method, make sure to check for
+  ;;       null pointers!
+   ; cmd-ln-t
+  ((config-ptr
+    :initarg :config-ptr)))
+
+(defgeneric option (config config-keyword)
+  ;; TODO: Maybe make this not `SETF'-able.  We'll see...
+  (:documentation "Return the config value indicated by
+  `CONFIG-KEYWORD'. This is a `SETF'-able place."))
+
+(defmethod option ((config config) config-keyword)
+  (let ((config-string (config-keyword->string config-keyword))
+	(config-value-type (gethash config-keyword +config-type-hash+)))
+    (with-slots (config-ptr) config
+      ;; FIXME:
+      ;; TODO: THIS MIGHT NOT BE PORTABLE SINCE CASE COMPARES WITH
+      ;;       EQL.
+      (case config-value-type
+	('integer (cmd-ln-int-r config-ptr config-string))
+	('float (cmd-ln-float-r config-ptr config-string))
+	('boolean (cmd-ln-boolean-r config-ptr config-string))
+	('string (cmd-ln-str-r config-ptr config-string))
+	(otherwise (progn
+		     (warn "Type of ~A is unknown; assuming ~A." config-keyword 'string)
+		     (cmd-ln-str-r config-ptr config-string)))))))
+
+(defun make-testing-config ()
+  ;; This is just for testing stuff
+  (make-instance 'config
+		 :config-ptr
+		 (cmd-ln-init (cffi:null-pointer)
+			      (ps-args)
+			      t
+			      :string "-hmm"
+			      :string "/usr/local/share/pocketsphinx/model/hmm/en_US/hub4wsj_sc_8k"
+			      :string "-lm"
+			      :string "/usr/local/share/pocketsphinx/model/lm/en_US/hub4.5000.DMP"
+			      :string "-dict"
+			      :string "/usr/local/share/pocketsphinx/model/lm/en_US/hub4.5000.dic"
+			      :pointer (cffi:null-pointer))))
+
+(defclass decoder ()
+  ;; TODO: Document
+   ; ps-decoder-t
+  ((decoder-ptr)))
+
+(defgeneric config (decoder)
+  (:documentation "Return the current `CONFIG' object for the decoder
+  `DECODER'."))
+
+(defmethod config ((decoder decoder))
+  (with-slots (decoder-ptr) decoder
+    (make-instance 'config :config-ptr (ps-get-config decoder-ptr))))
+
+(defmethod initialize-instance :after ((decoder decoder) &key config)
+  ;; TODO: Need to test these finalizers.
+  ;; TODO: Error checking/null-ptr checking.
+  (check-type config config)
+  ;; TODO: This _seriously_ needs null-pointer checking!!
+  (let ((decoder-ptr (ps-init (slot-value config 'config-ptr))))
+    (setf (slot-value decoder 'decoder-ptr) decoder-ptr)
+    (tg:finalize decoder (lambda () (ps-free decoder-ptr)))))
+
+(defclass hypothesis ()
+  ;; TODO: Document
+  ;; NOTE: This does not require a finalizer, because we can free the
+  ;;       pointers right after translation.
+  ((hyp-string
+    ; string
+    :initarg :hyp-string
+    :reader hyp-string)
+   (score
+    ; int32
+    :initarg :score
+    :reader score)
+   (utterance-id
+    ; string
+    :initarg :utterance-id
+    :reader utterance-id)))
+
+(defgeneric best-hypothesis (decoder)
+  (:documentation "Return a `HYPOTHESIS' object containing the best
+  hypothesis at this point in decoding, or `NIL' if no hypothesis is
+  available."))
+
+(defmethod best-hypothesis ((decoder decoder))
+  ;; TODO: Make this exception safe with respect to the foreign allocs.
+  ;; TODO: Fix and make sure this doesn't break with null-pointers.
+  (with-slots (decoder-ptr) decoder
+    (let* ((score-ptr (cffi:foreign-alloc :int32))
+	   (uttid-ptr (cffi:foreign-alloc :string))
+	   (hyp-string (ps-get-hyp decoder-ptr score-ptr uttid-ptr))
+	   (hypothesis (when (not (cffi:null-pointer-p hyp-string))
+			 (make-instance 'hypothesis
+					:hyp-string hyp-string
+					:score (cffi:mem-ref score-ptr :int32)
+					:utterance-id (cffi:mem-ref uttid-ptr :string)))))
+      (cffi:foreign-free score-ptr)
+      (cffi:foreign-free uttid-ptr)
+      hypothesis)))
+
 ;; stuff that I've typed into the REPL
 (defparameter *cmd-ln*
   (cmd-ln-init (cffi:null-pointer)
@@ -140,7 +300,8 @@ The form(s) before the located form."
 	       :string "-lm"
 	       :string "/usr/local/share/pocketsphinx/model/lm/en_US/hub4.5000.DMP"
 	       :string "-dict"
-	       :string "/usr/local/share/pocketsphinx/model/lm/en_US/hub4.5000.dic"))
+	       :string "/usr/local/share/pocketsphinx/model/lm/en_US/hub4.5000.dic"
+	       :pointer (cffi:null-pointer)))
 (print *cmd-ln*)
 
 (defparameter *decoder* (ps-init *cmd-ln*))

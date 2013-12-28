f5f4a72546cb5022badd5c24a61cbd5b183a062f
Matt Walker <matt.g.d.walker@gmail.com>
2013-12-27 20:43:07 -0500

    Added a skeleton file for the `SEGMENTATION' class, which corresponds to a ps_seg_t* in the C API.
     On branch master
    
    	new file:   src/segmentation.lisp

---
+++ src/segmentation.lisp
@@ -0,0 +1,12 @@
+(cl:in-package :ps)
+
+(defclass segmentation (ps-utils:iterator)
+  (seg-ptr))
+
+(defmethod next ((segmentation segmentation))
+  ;; body
+  )
+
+(defmethod value ((segmentation segmentation))
+  ;; body
+  )
